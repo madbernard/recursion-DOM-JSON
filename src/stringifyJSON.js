c48872244f263f859e8da99c789999df65cf7a94
Mad Bernard <madeline.a.bernard@gmail.com>
2015-10-21 10:43:55 -0700

    Beginning on stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,17 @@
 
 // but you don't so you're going to write it from scratch:
 
+// of the three basic things, JSON.stringify treats them like so,
+// string becomes "string", number becomes 3, boolean is true or false
+// everything that comes out of JSON.stringify is a string, "" around it
+
+// starting at Ben's suggestion by getting it to output those when input those
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeOf.obj === 'number' || typeOf.obj === 'boolean') {
+    return '"' + obj + '"';
+  }
+  if (typeOf.obj === 'string') {
+    return '""' + obj + '""';
+  }
 };
