2625a0fc2e037027167503d69e3d006de5cb1d63
Mad Bernard <madeline.a.bernard@gmail.com>
2015-10-21 15:22:49 -0700

    Fully working implementation! Tidied up my notes to self

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,49 +1,16 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
+/* In case, for extra credit, I wanted to make this perfectly match
+the MDN description of JSON.stringify, these are the parts that need doing:
 
-// but you don't so you're going to write it from scratch:
+ JSON.stringify() converts a value to JSON notation representing it:
 
-// of the three basic things, JSON.stringify treats them like so,
-// string becomes "string", number becomes 3, boolean is true or false
-// everything that comes out of JSON.stringify is a string, "" around it
-
-// starting at Ben's suggestion by getting it to output those when input those
-
-/*  Done things
- * Boolean, Number, and String objects are converted to the corresponding primitive
- values during stringification, in accord with the traditional conversion semantics.
- ===== done?
  * All symbol-keyed properties will be completely ignored, even when using the
  replacer function.
- ===== Doesn't need addressing in this implementation. ES6
-
-// need to address null
 
  * If undefined, a function, or a symbol is encountered during conversion it is
  either omitted (when it is found in an object) or censored to null (when it is found
  in an array).
- --- Set up if statement within if statement
- ===== do not need to address this special case for this exercise, may do later as extra credit
+
  */
-
-// what about other typeof results?
-// undefined = 'undefined', null = 'object', symbol = 'symbol', function = 'function'
-// everything else = 'object'
-
-// JSON.stringify wants:
-/* JSON.stringify() converts a value to JSON notation representing it:
-
- * Properties of non-array objects are not guaranteed to be stringified in any
- particular order. Do not rely on ordering of properties within the same object
- within the stringification.
- --- Use for/in
- * Non-enumerable properties will be ignored
- --- Use for/in
- */
-// change tostring prototype to save structural things like {} and null?
-
-// Here's my thought re: recursion doing this
-// find length, set that as a var, recurse up to that var
 
 var stringifyJSON = function(obj) {
   if (obj === null && typeof obj === 'object') {
@@ -71,7 +38,9 @@ var stringifyJSON = function(obj) {
   // object case
   var buildStringFromObject = [];
   for (var key in obj) {
-    buildStringFromObject.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    if (stringifyJSON(obj[key])) {
+      buildStringFromObject.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    }
   }
   return '{' + buildStringFromObject.join() + '}';
 };
