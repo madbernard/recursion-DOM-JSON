c1ae6a998f3ad759bb7647fb2bfe6a636873ce6c
Mad Bernard <madeline.a.bernard@gmail.com>
2015-10-23 13:03:28 -0700

    about to put a lot of functionality in the for loop

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -14,23 +14,37 @@ var getElementsByClassName = function(className) {
   var arrayOfBranchReturns = [];
 
 // make document.body a variable that can change?  or just loop on root[i].children?
-  var root = document.body.children;
+// http://youmightnotneedjquery.com/
+// everything jQuery all the time
+var recurseOn = document.body;
+var $kids = $(recurseOn).children();
+
+// bit that does the work
+// recurseon needs to be brought into jQuery
+if (recurseOn.hasClass(className)) {
+  arrayOfBranchReturns.push(recurseOn);
+}
 
 // need to set up base case that returns when there are no more children, if .hasClass(className)
 // https://developer.mozilla.org/en-US/docs/Web/API/Node/childNodes
+// termination condition/base case
 
-if (!root.hasChildNodes() && root.hasClass(className)) {
-  arrayOfBranchReturns.push(root);
-  return;
-}
-else {
+if (!recurseOn.hasChildNodes()) {
   return;
 }
 
-  for (var i = 0; i < root.length; ++i) {
-    arrayOfBranchReturns.push(getElementsByClassName(root[i]));
-      //return after pushing
-    }
+// recurse bit
 
-  return arrayOfBranchReturns;//array.concat stuff to make it flat
+for (var i = 0; i < $kids.length; ++i) {
+  // if there's something there, not just emptyarray
+  if (typeof recurseOn[i] !== 'undefined') {
+    getElementsByClassName();
+  }
+  // else end of branch
+  else {
+    return;
+  }
+}
+
+return arrayOfBranchReturns;//array.concat stuff to make it flat
 };
