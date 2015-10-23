6bf0973fc362ca4c2da5a4c8d022f9e93c93dd10
Mad Bernard <madeline.a.bernard@gmail.com>
2015-10-22 17:03:14 -0700

    start of structure of getElementsByClassName.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,33 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+
+// jQuery hasClass method
+
+var getElementsByClassName = function(className) {
+  //start at base, get array of children FUNC, recurse into that array,
+  //if at end of branch, check for the class name FUNC, if classname push into subarray return subarray?
+
+  var arrayOfBranchReturns = [];
+
+// make document.body a variable that can change?  or just loop on root[i].children?
+  var root = document.body.children;
+
+// need to set up base case that returns when there are no more children, if .hasClass(className)
+// https://developer.mozilla.org/en-US/docs/Web/API/Node/childNodes
+
+if (!root.hasChildNodes() && root.hasClass(className)) {
+  arrayOfBranchReturns.push(root);
+  return;
+}
+else {
+  return;
+}
+
+  for (var i = 0; i < root.length; ++i) {
+    arrayOfBranchReturns.push(getElementsByClassName(root[i]));
+      //return after pushing
+    }
+
+  return arrayOfBranchReturns;//array.concat stuff to make it flat
 };
