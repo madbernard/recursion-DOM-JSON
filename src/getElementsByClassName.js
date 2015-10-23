38636ccac9da2324db3cb8dd8dcab45c5a6f7973
Mad Bernard <madeline.a.bernard@gmail.com>
2015-10-23 14:46:09 -0700

    added default for rootNode, termination condition out of loop

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,39 +5,30 @@
 
 // But instead we're going to implement it from scratch:
 
-// jQuery hasClass method
-
-var getElementsByClassName = function(className) {
+var getElementsByClassName = function(rootNode, className) {
   //start at base, get array of children FUNC, recurse into that array,
   //if at end of branch, check for the class name FUNC, if classname push into subarray return subarray?
 
   var arrayOfBranchReturns = [];
 
-  // make document.body a variable that can change?  or just loop on root[i].children?
-  // http://youmightnotneedjquery.com/
-  // everything jQuery all the time
-  var $kids = $(document.body).children();
+  if (!rootNode) {
+    rootNode = document.body;
+  }
 
-  // need to set up base case that returns when there are no more children, if .hasClass(className)
-  // https://developer.mozilla.org/en-US/docs/Web/API/Node/childNodes
+  // if there's nothing in rootNode, end of branch, termination condition
+  if (rootNode.length === 0) {
+    return;
+  }
 
-
-  // recurse bit
+  var $kids = $(rootNode).children();
 
   for (var i = 0; i < $kids.length; ++i) {
-    // if there's something there, not just emptyarray
-    if (typeof $kids[i] !== 'undefined') {
-      // bit that does the work
-      if ($kids[i].hasClass(className)) {
-        arrayOfBranchReturns.push($kids[i]);
-      }
-      //go deeper
-      //how?
+    // bit that does the work
+    if ($kids.eq(i).hasClass(className)) {
+      arrayOfBranchReturns.push($kids[i]);
     }
-    // else end of branch, also termination condition/base case
-    else {
-      return;
-    }
+    //go deeper
+    getElementsByClassName($kids.eq(i), className);
   }
 
   return arrayOfBranchReturns;//array.concat stuff to make it flat
