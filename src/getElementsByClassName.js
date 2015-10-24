6ff5852cc82ddf3106f04a322f24a84a7b553138
Mad Bernard <madeline.a.bernard@gmail.com>
2015-10-23 20:50:39 -0700

    added a few notes, about to bring start case into loop

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -25,12 +25,13 @@ var getElementsByClassName = function(className, rootNode) {
   var $kids = $(rootNode).children();
 
   for (var i = 0; i < $kids.length; ++i) {
-    // bit that does the work
+    // bit that does the work.  $kids.eq(i) is like $kids[i] but brings element into jQuery
     if ($kids.eq(i).hasClass(className)) {
       arrayOfBranchReturns.push($kids[i]);
     }
     //go deeper
     var holder = getElementsByClassName(className, $kids.eq(i));
+    //store what has been learned
     arrayOfBranchReturns = arrayOfBranchReturns.concat(holder);
   }
 
